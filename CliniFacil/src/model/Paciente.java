45cab637d6e3c9945fdae425c055afd96760e8d3
silviamore <silviamore@ig.com.br>
2016-11-11 11:46:09 -0200

    #41

--- CliniFacil/src/model/Paciente.java
+++ CliniFacil/src/model/Paciente.java
@@ -1,7 +1,170 @@
 package model;
 
+import java.util.Date;
+
 public class Paciente {
     private int id;
     private String nome;
+    private Date dataNscimento;
+    private Sexo sexo;
+    private double peso;
+    private double altura;
+    private String racaCor;
+    private String cep;
+    private String endereco;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String telefone;
+    private String celular;
+    private String correioEletronico;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNscimento() {
+        return dataNscimento;
+    }
+
+    public void setDataNscimento(Date dataNscimento) {
+        this.dataNscimento = dataNscimento;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getRacaCor() {
+        return racaCor;
+    }
+
+    public void setRacaCor(String racaCor) {
+        this.racaCor = racaCor;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getCorreioEletronico() {
+        return correioEletronico;
+    }
+
+    public void setCorreioEletronico(String correioEletronico) {
+        this.correioEletronico = correioEletronico;
+    }
     
+       
 }
