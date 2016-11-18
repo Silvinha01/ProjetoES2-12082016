e7016bcc35bbb110bcf7898e4855774d0fae1764
silviamore <silviamore@ig.com.br>
2016-11-18 09:28:12 -0200

    #42

--- CliniFacil/src/model/Medico.java
+++ CliniFacil/src/model/Medico.java
@@ -2,7 +2,42 @@ package model;
 
 public class Medico {
     private int id;
+    private int crm;
     private String nome;
+    private String especialidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCrm() {
+        return crm;
+    }
+
+    public void setCrm(int crm) {
+        this.crm = crm;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }   
+    
     
 }
     
